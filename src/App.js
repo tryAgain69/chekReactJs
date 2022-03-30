1fe5d5f28a1c00f4e8eeb178d95cb0db8947db47
truAgain69 <montaa32@gmail.com>
2022-03-30 14:34:52 +0000

    ReactJs

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import FormExample from './components/form.js';
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <FormExample/>
+    </div>
+  );
+}
+
+export default App;
